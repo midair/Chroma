081f63ed3329133da7c2e94eff9ba223680c22a7
Claire Treyz <treyz@mit.edu>
2014-01-18 18:06:36 -0500

    initial commit

---
+++ Source/Gameplay.h
@@ -0,0 +1,22 @@
+//
+//  Gameplay.h
+//  ColorSpin
+//
+//  Created by Claire Treyz on 1/18/14.
+//  Copyright (c) 2014 Apportable. All rights reserved.
+//
+
+#import "CCNode.h"
+
+@interface Gameplay : CCNode
+
+typedef enum colorStateTypes
+{
+    RED,
+    ORANGE,
+    YELLOW,
+    GREEN,
+    BLUE,
+    VIOLET
+} ColorState;
+@end
---
+++ Source/Gameplay.m
@@ -0,0 +1,114 @@
+//
+//  Gameplay.m
+//  ColorSpin
+//
+//  Created by Claire Treyz on 1/18/14.
+//  Copyright (c) 2014 Apportable. All rights reserved.
+//
+
+#import "Gameplay.h"
+
+@implementation Gameplay {
+    CCNode *_background;
+    CCNode *_palette;
+    bool colorPicking;
+    ColorState colorState;
+    UITouch *colorPickTouch;
+}
+
+// is called when CCB file has completed loading
+-(void)didLoadFromCCB {
+    //tell this scene to accept touches
+    self.userInteractionEnabled = TRUE;
+}
+
+-(void) touchBegan:(UITouch *)touch withEvent:(UIEvent *)event
+{
+    CGPoint pointLocation = touch.locationInWorld;
+    if (!colorPicking) {        
+        CGPoint centered = ccpSub(pointLocation, ccp(59, 50));
+        if (ccpLength(centered)<40){
+            colorPicking = TRUE;
+            colorPickTouch = touch;
+        }
+        
+    }
+    if (TRUE) {
+        
+        
+        CGSize screenSize = [[CCDirector sharedDirector] viewSize];
+        
+        CGPoint centered = ccpSub(pointLocation, ccp(screenSize.width/2, screenSize.height/2));
+        
+        if (ccpLength(centered) < 160) {
+            float Q =  fmod(CC_RADIANS_TO_DEGREES(atan2(centered.y, centered.x)) + _background.rotation +360, 360);
+            if ((Q < 30) || (Q > 330)) {
+                CCLOG(@"RED");
+                colorState = RED;
+                [_palette setColor:[CCColor redColor]];
+                
+            }
+            else if (Q > 30 && Q < 90)
+            {
+                CCLOG(@"VIOLET");
+                colorState = VIOLET;
+                [_palette setColor:[CCColor purpleColor]];
+
+
+            }
+            else if (Q > 90 && Q < 150)
+            {
+                CCLOG(@"BLUE");
+                colorState = BLUE;
+                [_palette setColor:[CCColor blueColor]];
+
+            }
+            else if (Q > 150 && Q < 210)
+            {
+                CCLOG(@"GREEN");
+                colorState = GREEN;
+                [_palette setColor:[CCColor greenColor]];
+
+            }
+            else if (Q > 210 && Q < 270)
+            {
+                CCLOG(@"YELLOW");
+                colorState = YELLOW;
+                [_palette setColor:[CCColor yellowColor]];
+
+            }
+            else if (Q > 270 && Q < 330)
+            {
+                CCLOG(@"ORANGE");
+                colorState = ORANGE;
+                [_palette setColor:[CCColor orangeColor]];
+
+            }
+        }
+    }
+}
+
+-(void) touchMoved:(UITouch *)touch withEvent:(UIEvent *)event
+{
+
+}
+
+-(void) touchCancelled:(UITouch *)touch withEvent:(UIEvent *)event
+{
+    if (touch == colorPickTouch) {
+        colorPicking = FALSE;
+        colorPickTouch = nil;
+    }
+    
+    
+}
+
+-(void)touchEnded:(UITouch *)touch withEvent:(UIEvent *)event
+{
+    if (touch == colorPickTouch) {
+        colorPicking = FALSE;
+        colorPickTouch = nil;
+    }
+}
+
+@end
